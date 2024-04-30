8f3d43d1e4efd46bfae260afe3fa9cca214fec72
Tundephilps <tundephilps@gmail.com>
2024-04-30 16:31:19 +0100

    Shipping and Payment Page

--- lib/hooks/useCartStore.ts
+++ lib/hooks/useCartStore.ts
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 import { round2 } from "../utils";
-import { OrderItem } from "../models/OrderModel";
+import { OrderItem, ShippingAddress } from "../models/OrderModel";
 
 import { persist } from "zustand/middleware";
 
@@ -10,6 +10,8 @@ type Cart = {
   taxPrice: number;
   shippingPrice: number;
   totalPrice: number;
+  paymentMethod: string;
+  shippingAddress: ShippingAddress;
 };
 const initialState: Cart = {
   items: [],
@@ -17,6 +19,14 @@ const initialState: Cart = {
   taxPrice: 0,
   shippingPrice: 0,
   totalPrice: 0,
+  paymentMethod: "PayPal",
+  shippingAddress: {
+    fullName: "",
+    address: "",
+    city: "",
+    postalCode: "",
+    country: "",
+  },
 };
 
 export const cartStore = create<Cart>()(
@@ -26,14 +36,23 @@ export const cartStore = create<Cart>()(
 );
 
 export default function useCartService() {
-  const { items, itemsPrice, taxPrice, shippingPrice, totalPrice } =
-    cartStore();
+  const {
+    items,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+    paymentMethod,
+    shippingAddress,
+  } = cartStore();
   return {
     items,
     itemsPrice,
     taxPrice,
     shippingPrice,
     totalPrice,
+    paymentMethod,
+    shippingAddress,
     increase: (item: OrderItem) => {
       const exist = items.find((x) => x.slug === item.slug);
       const updatedCartItems = exist
@@ -69,6 +88,16 @@ export default function useCartService() {
         shippingPrice,
         taxPrice,
         totalPrice,
+      });
+    },
+    saveShippingAddrress: (shippingAddress: ShippingAddress) => {
+      cartStore.setState({
+        shippingAddress,
+      });
+    },
+    savePaymentMethod: (paymentMethod: string) => {
+      cartStore.setState({
+        paymentMethod,
       });
     },
   };
